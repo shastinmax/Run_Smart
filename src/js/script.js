c81a74ec33deea6932296de45c32d945844a7b21
Maxim Shastin <shastin_maksim@mail.ru>
2024-02-12 18:33:33 +0300

    add section carousel

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+		speed: 1200,
+		adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-solid.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right-solid.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+	});
+});

60e90543c06e72c0a3bab1a996b888343052e499
Maxim Shastin <shastin_maksim@mail.ru>
2024-02-13 13:01:14 +0300

    add section catalog

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,22 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog__content-item').eq(i).toggleClass('catalog__content-item_active');
+				$('.catalog__content-list').eq(i).toggleClass('catalog__content-list_active');
+			})
+		});
+	};
+
+	toggleSlide('.catalog__link');
+	toggleSlide('.catalog__link-back');
 });

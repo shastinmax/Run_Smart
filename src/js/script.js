af19456c12e89927d79046faa67007716d7ff65c
Maxim Shastin <shastin_maksim@mail.ru>
2024-02-16 10:28:59 +0300

    add btn page up

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,42 @@ $(document).ready(function(){
 
 	toggleSlide('.catalog__link');
 	toggleSlide('.catalog__link-back');
+	//modal
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay,#consultation').fadeIn()
+	})
+	$('.modal__close').on('click', function () {
+		$('.overlay,#consultation,#thanks,#order').fadeOut()
+	})
+	$('.button_buy').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__desc').text($('.catalog__subtitle').eq(i).text())
+			$('.overlay,#order').fadeIn()
+		})
+	})
 });
+
+const btnUp = {
+	el: document.querySelector('.btn-up'),
+	show() {
+		this.el.classList.remove('btn-up_hide');
+	},
+	hide() {
+		this.el.classList.add('btn-up_hide');
+	},
+	addEventListener() {
+		window.addEventListener('scroll', () => {
+			const scrollY = window.scrollY || document.documentElement.scrollTop;
+			scrollY > 400 ? this.show() : this.hide();
+		});
+		document.querySelector('.btn-up').onclick = () => {
+			window.scrollTo({
+				top: 0,
+				left: 0,
+				behavior: 'smooth'
+			});
+		}
+	}
+}
+
+btnUp.addEventListener();
